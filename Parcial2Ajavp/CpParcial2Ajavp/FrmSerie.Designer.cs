b9b919aa013820d5342c7cb5c008223880577db5
vera-alex <alexjorgeabelverapinto@gmail.com>
2024-06-02 20:50:18 -0400

    instalación de paquetes NuGet

---
+++ Parcial2Ajavp/CpParcial2Ajavp/FrmSerie.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace CpParcial2Ajavp
+{
+    partial class FrmSerie
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmSerie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1100, 675);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmSerie";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "::: Parcial - 2 :::";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
